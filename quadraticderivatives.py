45789979b5c251c2ff32730b5dd88d3e2c018024
Malhar <51699964+malharb@users.noreply.github.com>
2019-06-15 09:25:42 -0700

    Add files via upload

---
+++ quadraticderivatives.py
@@ -0,0 +1,23 @@
+#calculating the slope at any point in a quadratic function
+
+print("The form of a quadratic function isy = ax^2 + bx + c")
+a_value = input("Enter the 'a' value: ")
+a_value = int(a_value)
+
+b_value = input("Enter the 'b' value: ")
+b_value = int(b_value)
+
+c_value = input("Enter the 'c' value: ")
+c_value = int(c_value)
+
+a_derivative = a_value * 2
+b_derivative = b_value * 1
+
+print(f'The equation for the slop is {a_derivative}x + {b_derivative}')
+x_value = input("For which x value do you want to find the slope?: ")
+x_value = float(x_value)
+
+slopei = (a_derivative * x_value)
+slope = slopei + b_derivative
+
+print(f'The slope when x is {x_value} is {slope}')
